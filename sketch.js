234fbfcead1cbd513243fa09251401bc4ea63509
ryansilveira72-sys <ryansilveira72@gmail.com>
2026-09-24 19:55:21 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,34 @@
+//variáveis 
 let trex;
 let trexRunning;
+let ground;
 
+//pré carregamentos
 function preload(){
   trexRunning = loadAnimation ("trex1.png","trex2.png","trex3.png");
 }
   
 
-
+//configuração inicial
 function setup(){
 createCanvas(600,200);
-trex = createSprite (50,160,20,50);
+trex = createSprite (50,160,20,50); 
   trex.addAnimation ("running",trexRunning);
-}
+  trex.scale = 0.5;
+  trex.x = 50;
 
+  ground = createSprite(200,180,400,20);
+  
+}
+//desenha os paranaue tudo
 function draw(){
   background ("white");
+
+  if(keyDown("space")){ 
+    trex.velocityY = -10
+  }
+
+  trex.velocityY += 0.8;
+  
   drawSprites ();
 }
